dc1ed946ff7d0695b65d16cb34f6acaae935d0dc
Ria Jha <ria.jha@ocado.com>
2018-04-18 14:30:01 +0100

    added reference to new branch

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ gem install sass --version 3.3.4
 rbenv rehash
 pip install git+https://github.com/PyGithub/PyGithub.git@ba50af5
 
-pip install -t lib codeforlife-portal aimmo
+pip install -t lib aimmo
+pip install -t lib git+https://github.com/riaJha97/codeforlife-portal.git@test_signup
 
 python get_latest_aimmo_unity_release.py
 
