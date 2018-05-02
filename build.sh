482b1a7f7103c60d490b8b93a7e13b026c1516d1
Ria Jha <ria.jha@ocado.com>
2018-05-02 12:16:58 +0100

    Changed build script to point to fix avatar marker branch

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rbenv rehash
 pip install git+https://github.com/PyGithub/PyGithub.git@ba50af5
 
 pip install -t lib aimmo
-pip install -t lib git+https://github.com/riaJha97/aimmo.git@test_ssl
+pip install -t lib git+https://github.com/riaJha97/aimmo.git@fix_avatar_marker
 
 python get_latest_aimmo_unity_release.py
 
