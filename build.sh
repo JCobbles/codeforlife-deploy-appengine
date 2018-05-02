e4e127670ccdf77ab7a26b288d1a0eea1d33ef22
Ria Jha <ria.jha@ocado.com>
2018-05-02 13:25:12 +0100

    Now installing portal

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ gem install sass --version 3.3.4
 rbenv rehash
 pip install git+https://github.com/PyGithub/PyGithub.git@ba50af5
 
-pip install -t lib aimmo
+pip install -t lib codeforlife-portal
 pip install -t lib git+https://github.com/riaJha97/aimmo.git@fix_avatar_marker
 
 python get_latest_aimmo_unity_release.py
