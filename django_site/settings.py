38f4a21992b43f5aa2f253b8e4648a15334f3154
Olaf Szmidt <szmidtolaf@gmail.com>
2018-05-16 11:12:41 +0100

    django_forms_bootstrap changed to the official naming

--- django_site/settings.py
+++ django_site/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django_countries',
-    'django_forms_bootstrap',
+    'django-forms-bootstrap',
     'rest_framework',
     'online_status',
 
