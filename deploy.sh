e382a519fccf4ed0f8f49c31f0b7c5e32f1aff04
Ria Jha <ria.jha@ocado.com>
2018-05-21 14:38:28 +0100

    Running migrations normally again

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ ${GCLOUD} container clusters get-credentials ${MODULE_NAME} --zone europe-west1-
 # Deploy the correct kubernetes cluster.
 python clusters_setup/deploy.py "${MODULE_NAME}"
 
-./manage.py migrate portal 0054_pending_join_request_can_be_blank --noinput
+./manage.py migrate --noinput
 
 envsubst <app.yaml.tmpl >app.yaml
 
